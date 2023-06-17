056a402630dbdf3c196a3c7a486123c6b7c77abe
terwer <youweics@163.com>
2023-06-17 11:49:22 +0800

    feat: init api middleware go

--- api/index.go
+++ api/index.go
@@ -1,10 +1,61 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+// RequestBody {"md":"# test"}
+type RequestBody struct {
+	Md string `json:"md"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Invalid request method, POST only")
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error reading request body")
+		return
+	}
+
+	var requestBody RequestBody
+	err = json.Unmarshal(body, &requestBody)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error parsing JSON request body")
+		return
+	}
+
+	fmt.Fprintf(w, "md: %s\n", requestBody.Md)
+
+	//// get post parameters
+	//err := r.ParseForm()
+	//if err != nil {
+	//	fmt.Println("ParseForm Error: ", err)
+	//}
+	//
+	//name := r.PostFormValue("name")
+	//age := r.PostFormValue("age")
+	//fmt.Fprintf(w, "Name: %s\nAge: %s\n", name, age)
+	//
+	//luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
+	//html := luteEngine.MarkdownStr("terwer", "**Lute** - A structured markdown engine.")
+	//
+	//ret := gulu.Ret.NewResult()
+	//ret.Data = html
+	//
+	//// Marshal
+	//output, err := sonic.Marshal(&ret)
+	//if err != nil {
+	//	ret.Msg = err.Error()
+	//}
+	//fmt.Fprintf(w, string(output))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/terwer/api-middleware-go
 
 go 1.20
+
+require (
+	github.com/88250/gulu v1.2.2 // indirect
+	github.com/88250/lute v1.7.5 // indirect
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+)
