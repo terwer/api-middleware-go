321d7c6ddc62bd7585ba4458e36c0270bcd213a5
terwer <youweics@163.com>
2023-06-17 12:10:08 +0800

    feat: init api middleware go

--- api/index.go
+++ api/index.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/88250/gulu"
 	"github.com/88250/lute"
@@ -15,42 +14,71 @@ type RequestBody struct {
 	Md string `json:"md"`
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func handleMarkdown(w http.ResponseWriter, r *http.Request) {
+	ret := gulu.Ret.NewResult()
+
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "Invalid request method, POST only")
+		ret.Code = http.StatusMethodNotAllowed
+		ret.Msg = "Invalid request method, POST only"
+		resultBytes, _ := sonic.Marshal(ret)
+		fmt.Fprintf(w, "%s", resultBytes)
 		return
 	}
 
-	// get post parameters
-	// 50MB 最大大小
+	// Get post parameters
 	maxBodySize := int64(50 * 1024 * 1024)
 	body, err := io.ReadAll(io.LimitReader(r.Body, maxBodySize))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Error reading request body")
+		ret.Msg = "Error reading request body"
+		resultBytes, _ := sonic.Marshal(ret)
+		fmt.Fprintf(w, "%s", resultBytes)
 		return
 	}
 
 	var requestBody RequestBody
-	err = json.Unmarshal(body, &requestBody)
+	err = sonic.Unmarshal(body, &requestBody)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Error parsing JSON request body")
+		ret.Msg = "Error parsing JSON request body"
+		resultBytes, _ := sonic.Marshal(ret)
+		fmt.Fprintf(w, "%s", resultBytes)
 		return
 	}
 	fmt.Fprintf(w, "md: %s\n", requestBody.Md)
 
-	luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
-	html := luteEngine.MarkdownStr("terwer", "**Lute** - A structured markdown engine.")
+	luteEngine := lute.New()
+	html := luteEngine.MarkdownStr(requestBody.Md, "**Lute** - A structured markdown engine.")
 
-	ret := gulu.Ret.NewResult()
 	ret.Data = html
 
 	// Marshal
-	output, err := sonic.Marshal(&ret)
+	output, err := sonic.Marshal(ret)
 	if err != nil {
 		ret.Msg = err.Error()
+		resultBytes, _ := sonic.Marshal(ret)
+		fmt.Fprintf(w, "%s", resultBytes)
+		return
 	}
-	fmt.Fprintf(w, string(output))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(ret.Code)
+	fmt.Fprintf(w, "%s", output)
+}
+
+func handleUnknownType(w http.ResponseWriter, r *http.Request) {
+	ret := gulu.Ret.NewResult()
+	ret.Code = http.StatusBadRequest
+	ret.Msg = "Unknown type parameter"
+	resultBytes, _ := sonic.Marshal(ret)
+	fmt.Fprintf(w, "%s", resultBytes)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Get type parameter
+	t := r.URL.Query().Get("type")
+	switch t {
+	case "md":
+		handleMarkdown(w, r)
+	default:
+		handleUnknownType(w, r)
+	}
 }
